8fb5d3906aeca32966995bb890b43c9984b712ce
Mark Steve Samson <hello@marksteve.com>
2015-07-01 19:40:42 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import random
+
+from flask import Flask, request
+
+random.seed()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def roll():
+  resp = ""
+  dice = request.form['text'].split()
+  for i, die in enumerate(dice):
+    resp += "Dice {}\n".format(i + 1)
+    rolls, faces = map(int, die.split('d'))
+    for j in range(rolls):
+      roll = random.randint(1, faces)
+      resp += "  Roll {}: {}\n".format(j + 1, roll)
+  return resp
+
+
+app.run(host="0.0.0.0", port=os.environ["POST"])
